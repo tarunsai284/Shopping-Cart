c194210f4b5f091a00b3cc64611397d945ea87bd
Tarun <tarunsai.ponnam@phenompeople.com>
2018-06-13 16:05:49 +0530

    Initial Commit

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector:'sh-products',
+    templateUrl: './product-list.component.html',
+})
+export class ProductListComponent{
+    productPanelHeading: string = 'Product List';
+}
